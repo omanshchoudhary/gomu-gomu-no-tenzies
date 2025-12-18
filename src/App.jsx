66ea2f2c9ece69127f174c665ae090b84b39a656
Omansh Choudhary <omansh.choudhary@gmail.com>
2025-12-19 02:27:56 +0530

    Add random value die

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import './App.css'
-import Die from './components/Die'
+import "./App.css";
+import Die from "./components/Die";
+import { useState } from "react";
 function App() {
+  const [dice, setDice] = useState(generateRandomDice())
 
+  function generateRandomDice() {
+    let randomValues = [];
+    for (let i = 0; i < 10; i++) {
+      const value = Math.floor(Math.random() * 6) + 1;
+      randomValues.push(value);
+    }
+    return randomValues
+  }
+
+  const diceElements = dice.map(num => <Die value={num} />)
   return (
-    <main className='main-section'>
-      <div className='dice-grid'>
-      <Die value={1}></Die>
-      <Die value={1}></Die>
-      <Die value={1}></Die>
-      <Die value={1}></Die>
-      <Die value={1}></Die>
-      <Die value={1}></Die>
-      <Die value={1}></Die>
-      <Die value={1}></Die>
-      <Die value={1}></Die>
-      <Die value={1}></Die>
-      </div>
+    <main className="main-section">
+      <div className="dice-grid">{diceElements}</div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
